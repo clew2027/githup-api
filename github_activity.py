bce33be67a03891cd79ea4bdf31a3a1907b9cef4
clew2027 <clew27@seas.upenn.edu>
2026-09-22 21:26:55 -0400

    init

--- github_activity.py
+++ github_activity.py
@@ -1,4 +1,5 @@
 import urllib.request
+import urllib.error
 import sys
 import json
 import ast 
@@ -6,25 +7,29 @@ import ast
 user = sys.argv[1]
 url = f"https://api.github.com/users/{user}/events"
 
-with urllib.request.urlopen(url) as response:
-    raw_data = response.read()
-data = raw_data.decode("utf-8")
-data = json.loads(data)
+try: 
+    with urllib.request.urlopen(url) as response:
+        raw_data = response.read()
+    data = raw_data.decode("utf-8")
+    data = json.loads(data)
 
-result = []
-for d in data:
-    if d["type"] == "PushEvent":
-        repo_name = d["repo"]["name"]
-        string = f"Pushed commits to {repo_name}"
-        found = False
-        for r in result:
-            if r[0] == string:
-                r[1] += 1
-                found = True
-                break
-        if not found:
-            result.append([string, 1])
+    result = []
+    for d in data:
+        if d["type"] == "PushEvent":
+            repo_name = d["repo"]["name"]
+            string = f"Pushed commits to {repo_name}"
+            found = False
+            for r in result:
+                if r[0] == string:
+                    r[1] += 1
+                    found = True
+                    break
+            if not found:
+                result.append([string, 1])
 
-for r in result:
-    print(r)
+    for r in result:
+        print(r)
     
+
+except urllib.error.HTTPError:
+    print("Github User Not Found")
